44a376955ceebe0cc60bcaa1cba650009ce42c0c
jimmyhu <60071jimmy@gmail.com>
2021-06-20 14:45:00 +0800

    Update basic_functions.c
    Update basic_functions.c
    Add GetPlaneR, GetPlaneG, GetPlaneB, etc. functions

--- C/basic_functions.c
+++ C/basic_functions.c
@@ -16,6 +16,58 @@ void DeleteMonoImage(MONOIMAGE image)
     return;
 }
 
+MONOIMAGE GetPlaneR(const RGBIMAGE image)
+{
+    MONOIMAGE output = CreateMonoImage(image.XSIZE, image.YSIZE);
+    for (size_t y = 0; y < image.YSIZE; y++)
+    {
+        for (size_t x = 0; x < image.XSIZE; x++)
+        {
+            output.IMAGE_DATA[GetMonoImageIndex(x, y, output)] = 
+                image.IMAGE_DATA[GetRGBImageIndex(x, y, image)]->channels[0];
+        }
+    }
+    return output;
+}
+
+MONOIMAGE GetPlaneG(const RGBIMAGE image)
+{
+    MONOIMAGE output = CreateMonoImage(image.XSIZE, image.YSIZE);
+    for (size_t y = 0; y < image.YSIZE; y++)
+    {
+        for (size_t x = 0; x < image.XSIZE; x++)
+        {
+            output.IMAGE_DATA[GetMonoImageIndex(x, y, output)] = 
+                image.IMAGE_DATA[GetRGBImageIndex(x, y, image)]->channels[1];
+        }
+    }
+    return output;
+}
+
+MONOIMAGE GetPlaneB(const RGBIMAGE image)
+{
+    MONOIMAGE output = CreateMonoImage(image.XSIZE, image.YSIZE);
+    for (size_t y = 0; y < image.YSIZE; y++)
+    {
+        for (size_t x = 0; x < image.XSIZE; x++)
+        {
+            output.IMAGE_DATA[GetMonoImageIndex(x, y, output)] = 
+                image.IMAGE_DATA[GetRGBImageIndex(x, y, image)]->channels[2];
+        }
+    }
+    return output;
+}
+
+size_t GetMonoImageIndex(const size_t x, const size_t y, const MONOIMAGE image)
+{
+    return y * image.XSIZE + x;
+}
+
+size_t GetRGBImageIndex(const size_t x, const size_t y, const RGBIMAGE image)
+{
+    return y * image.XSIZE + x;
+}
+
 size_t clip(const size_t input, const size_t lowerbound, const size_t upperbound)
 {
     if (input < lowerbound)
